90b588067decad6a0087d81adb3ab133cbc22416
Ravindu Karunathilake <karunath@ualberta.ca>
2023-12-23 21:16:54 -0700

    Implemented Lox scanner and started the compiler implementation. Chapter 16.

--- main.c
+++ main.c
@@ -1,39 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
 
+#define BINPUT 1024
+
+static void repl(){
+  char line[BINPUT];
+  for(;;){
+    printf("> ");
+
+    if(!fgets(line, sizeof(line), stdin)){
+      printf("\n");
+      break;
+    }
+    interpret(line);
+  }
+}
+
+static char* readFile(const char* path){
+  FILE* file = fopen(path, "rb");
+
+  /* Move the file pointer to the end of file */ 
+  fseek(file, 0L, SEEK_END);
+  if(file == NULL){
+    fprintf(stderr, "Could not open file \"%s\".\n", path);
+    exit(74);
+  }
+
+
+  size_t fileSize = ftell(file);
+  /* Move the file pointer to the beginning of the file */ 
+  rewind(file); 
+
+  char* buffer = (char*) malloc(fileSize + 1); // +1 for null byte 
+  if(buffer == NULL){
+    fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+    exit(74);
+  }
+
+  size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+  if(bytesRead < fileSize){
+    fprintf(stderr, "Could not read file \"%s\".\n", path);
+    exit(74);
+  }
+
+  buffer[bytesRead] = '\0';
+
+  return buffer; 
+}
+
+static void runFile(const char* path){
+  char* source = readFile(path);
+  InterpretResult result = interpret(source);
+  free(source); 
+  
+  if(result == INTERPRET_COMPILE_ERROR){
+    exit(65);
+  }
+
+  if(result == INTERPRET_RUNTIME_ERROR){
+    exit(70);
+  }
+}
+
 int main(int argc, const char* argv[]){
   initVM();
-  Chunk chunk;
-  initChunk(&chunk);
- 
-  /* Insert 1.2 */
-  int index = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, index, 123);
   
-  /* Insert 3.4 */
-  index = addConstant(&chunk, 3.4);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, index, 123);
+  if(argc == 1){
+    repl();
+  }else if(argc == 2){
+    runFile(argv[1]);
+  }else{
+    fprintf(stderr, "Usage: clox [path]\n");
+    exit(64);
+  }
 
-  /* + */
-  writeChunk(&chunk, OP_ADD, 123);
-
-  /* Insert 5.6 */
-  index = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, index, 123);
-
-  writeChunk(&chunk, OP_DIVIDE, 123);
-  writeChunk(&chunk, OP_NEGATE, 123);
-  writeChunk(&chunk, OP_RETURN, 123);
-  //disassembleChunk(&chunk, "test chunk");
-
-  /* Execute the bytecode through the VM */
-  interpret(&chunk);
   freeVM();
-  freeChunk(&chunk);
   return(0);
 }
